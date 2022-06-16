032a4303089b205037c970027c50e0b9cbd7c775
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-06-16 15:39:44 +0300

    Update contacts-operations.js

--- src/redux/contacts/contacts-operations.js
+++ src/redux/contacts/contacts-operations.js
@@ -1,3 +1,4 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import actions from './contacts-action';
 
@@ -12,16 +13,37 @@ export const fetchContacts = () => async dispatch => {
     dispatch(actions.fetchContactError(error));
   }
 };
+// export const fetchContacts2 = createAsyncThunk(
+//   'contacts/fetchContacts',
+//   async () => {
+//     const { data } = await axios.get(
+//       'https://62a74e8cbedc4ca6d7c6c8ec.mockapi.io/contacts'
+//     );
+//     return data;
+//   }
+// );
 export const addContact =
   ({ name, phone }) =>
   dispatch => {
-    const contact = { name, phone };
+    
     dispatch(actions.addContactRequest());
     axios
       .post('https://62a74e8cbedc4ca6d7c6c8ec.mockapi.io/contacts', contact)
       .then(({ data }) => dispatch(actions.addContactSuccess(data)))
       .catch(error => dispatch(actions.addContactError(error)));
   };
+
+  // export const addContact2 = createAsyncThunk(
+  //   'contacts/addContacts',
+  //   async ({ name, phone }) => {
+  //     const contact = { name, phone };
+  //     axios
+  //     .post('https://62a74e8cbedc4ca6d7c6c8ec.mockapi.io/contacts', contact)
+  //     .then(({ data }) => addContactSuccess(data))
+  //     .catch(error => addContactError(error));
+  //     return 
+  //   }
+  // );
 
 export const deleteContact = id => dispatch => {
   dispatch(actions.deleteContactRequest());
